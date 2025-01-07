0dc831ddb06b2e1713b7879487c5263f72b551d4
Raef <ra.f@icloud.com>
2025-01-07 16:12:28 +0330

    Update Some Contents

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
 	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-8deg)' },
+          '50%': { transform: 'rotate(8deg)' },
+        },
+        animation: {
+          wiggle: 'wiggle 1s ease-in-out infinite',
+        }
+      }
+    },
 	},
 	plugins: [require("@tailwindcss/typography")],
 };
